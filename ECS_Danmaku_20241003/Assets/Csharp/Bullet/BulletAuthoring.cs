9120ff0303f1e1f0d3364408a697554d2c20d067
20240710_PC <game.232e013@gmail.com>
2024-11-09 08:25:21 +0900

    20241109_PL、弾、敵の移動可能範囲を実装。弾と敵は範囲外で削除

--- ECS_Danmaku_20241003/Assets/Csharp/Bullet/BulletAuthoring.cs
+++ ECS_Danmaku_20241003/Assets/Csharp/Bullet/BulletAuthoring.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 using static BulletHelper;
@@ -7,29 +5,9 @@ using static EntityCampsHelper;
 using static EntityCategoryHelper;
 
 #if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
 #endif
-
-/// <summary>
-/// 弾の情報
-/// </summary>
-public struct BulletData : IComponentData
-{
-    [Tooltip("寿命")]
-    public readonly float lifeTime;
-
-    [Tooltip("経過時間")]
-    public float elapsed;
-
-    /// <summary>
-    /// 弾の情報
-    /// </summary>
-    /// <param name="lifeTime">寿命</param>
-    public BulletData(float lifeTime)
-    {
-        this.lifeTime = lifeTime;
-        elapsed = 0.0f;
-    }
-}
 
 /// <summary>
 /// 弾の設定
@@ -41,9 +19,6 @@ public class BulletAuthoring : MonoBehaviour
 
     [SerializeField, Min(0.0f), Header("ダメージ量")]
     private float _damageAmount = 0.0f;
-
-    [SerializeField, Min(0.0f), Header("寿命")]
-    private float _lifeTime = 0.0f;
 
     [SerializeField, Min(UNLIMITED_PIERCE_MINIMUM), Header("残り貫通回数（下限 = 無限）")]
     private int _remainingPierceCount = 0;
@@ -61,7 +36,7 @@ public class BulletAuthoring : MonoBehaviour
             var entity = GetEntity(TransformUsageFlags.Dynamic);
 
             // 残り貫通回数が下限ではなかった
-            if (src._remainingPierceCount != BulletAuthoring.UNLIMITED_PIERCE_MINIMUM)
+            if (src._remainingPierceCount != UNLIMITED_PIERCE_MINIMUM)
             {
                 // 残り貫通回数のDataをアタッチ
                 AddComponent(entity, new RemainingPierceCountData(src._remainingPierceCount));
@@ -69,12 +44,11 @@ public class BulletAuthoring : MonoBehaviour
 
             AddComponent(entity, new BulletTag());
             AddComponent(entity, new DestroyableData());
-            AddComponent(entity, new BulletData(src._lifeTime));
             AddComponent(entity, new BulletIDealDamageData(src._damageAmount, src._campsType));
 
             // 陣営とカテゴリのTagをアタッチ
-            AddComponent(entity, EntityCampsHelper.GetCampsTagType(src._campsType));
-            AddComponent(entity, EntityCategoryHelper.GetCategoryTagType(src._entityCategory));
+            AddComponent(entity, GetCampsTagType(src._campsType));
+            AddComponent(entity, GetCategoryTagType(src._entityCategory));
         }
     }
 }
