0d95fe4e74aaa8d0e09772560fdcdf39b09cf2a7
20240710_PC <game.232e013@gmail.com>
2024-11-10 13:49:20 +0900

    20241110_タイトルロゴとそのアニメーションを作成。敵生成情報のScriptableObjectを作成

--- ECS_Danmaku_20241003/Assets/Csharp/Enemy/EnemyHelper.cs
+++ ECS_Danmaku_20241003/Assets/Csharp/Enemy/EnemyHelper.cs
@@ -20,49 +20,4 @@ static public class EnemyHelper
     /// 敵
     /// </summary>
     public struct EnemyTag : IComponentData{ }
-
-    /// <summary>
-    /// 敵の生成に必要な情報
-    /// </summary>
-    public struct EnemySpawnerData : IComponentData
-    {
-        [Tooltip("生成するPrefabのEntity")]
-        public readonly Entity enemyEntity;
-
-        [Tooltip("生成後の移動方向")]
-        public readonly DirectionTravelType directionTravelType;
-        
-        [Tooltip("生成する線の始点")]
-        public readonly Vector3 startPoint;
-
-        [Tooltip("生成する線の終点")]
-        public readonly Vector3 endPoint;
-
-        /// <summary>
-        /// 敵の生成に必要な情報
-        /// </summary>
-        /// <param name="enemyEntity">生成するPrefabのEntity</param>
-        /// <param name="directionTravelType">生成後の移動方向</param>
-        /// <param name="startPoint">生成する線の始点</param>
-        /// <param name="endPoint">生成する線の終点</param>
-        public EnemySpawnerData(
-            Entity enemyEntity,
-            DirectionTravelType directionTravelType,
-            Vector3 startPoint, 
-            Vector3 endPoint)
-        {
-            this.enemyEntity = enemyEntity;
-            this.directionTravelType = directionTravelType;
-            this.startPoint = startPoint;
-            this.endPoint = endPoint;
-        }
-    }
-
-    public class EnemyHealthPointDataDic
-    {
-        // あとで修正する。とりあえずの奴
-        // BurstCompile属性を付与している場所から呼び出した場合はエラーになる
-
-        static public Dictionary<int, List<Entity>> entitys = new();
-    }
 }
--- ECS_Danmaku_20241003/Assets/Csharp/Enemy/EnemySpawner/EnemySpawnerSystem.cs
+++ ECS_Danmaku_20241003/Assets/Csharp/Enemy/EnemySpawner/EnemySpawnerSystem.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using static EnemyHelper;
 using static MoveHelper;
+using static UnityEngine.EventSystems.EventTrigger;
 
 /// <summary>
 /// 敵の生成処理
@@ -17,25 +18,131 @@ using static MoveHelper;
 [BurstCompile]
 public partial struct EnemySpawnerSystem : ISystem
 {
-    private float _delta;
+    private float _elapsed;
+
+    [Tooltip("現在の生成要素番号")]
+    private int _currentSpawnNumber;
+
+    [Tooltip("EnemySpawnerSystemが有効か")]
+    public bool isSelfEnable;
 
     public void OnUpdate(ref SystemState state)
     {
-        Debug.Log("試験的なコードの為、後で直す");
-        // 現状は敵を1秒置きに生成しているので生成方法を変える必要がある
-        // 現状はDataを全て取得して生成しているので、敵の数が増えたら一回に敵の数生成することになってしまう
-        _delta += SystemAPI.Time.DeltaTime;
-        if (_delta < 1.0f) { return; }
-        _delta = 0.0f;
+        if (!SystemAPI.HasSingleton<EnemyEntitySingletonData>()) { return; }
 
-        var entityManager = state.EntityManager;
+        Debug.Log("SceneTag追加");
+        Debug.Log("複数回Setしてしまっている");
+        Debug.Log("GetSingletonでいい。ループはしなくていい");
 
-        // CommandBufferを作成
+
+
+        // シーンに配置した奴でも駄目だった
+        // Prefabとか、シーンに配置しているかとかは関係なさそう
+        // var enemy = ecb.Instantiate(enemyEntity);
+        // var enemy = entityManager.Instantiate(enemyEntity);
+        // entity取得側に問題？LogではEntityが存在していたが、どっかで破棄されている？
+        // インスタンスが違う？いや、取得自体はできている
+        // 反映されていない？
+
+        // Singleton.InstanceはSubSceneでは使えない
+        // Entity化した敵はSingletonDataに保持させるべき
+        Debug.LogError("Entity化した敵はSingletonDataに保持させるべき");
+
+        // なんか!isSelfEnableができていない
+        // 外部から変更した場合、変更した後に保存する必要があるのかも
+
+        // EnemySpawnerSystemが有効ではなかった
+        //if (!isSelfEnable) { return; }
+
+        // 経過時間にフレーム秒数を加算代入
+        //_elapsed += SystemAPI.Time.DeltaTime;
+
+        //if (!SystemAPI.HasSingleton<EnemySpawnerSingletonData>()) { return; }
+        //var enemySpawnerSingleton = SystemAPI.GetSingleton<EnemySpawnerSingletonData>();
+        //Entity enemySpawnerEntity = SystemAPI.GetSingletonEntity<EnemySpawnerSingletonData>();
+
+        //var enemySpawnInfos = enemySpawnerSingleton.enemySpawnInfos;
+        //var currentEnemySpawnInfo = enemySpawnInfos[_currentSpawnNumber];
+
+        //if (currentEnemySpawnInfo.CreationDelay > _elapsed) { return; }
+        //_currentSpawnNumber++;
+
+        //if (!SystemAPI.HasSingleton<EnemyEntitySingletonData>()) { return; }
+        //var enemyEntitySingleton = SystemAPI.GetSingleton<EnemyEntitySingletonData>();
+        //var enemyEntity = enemyEntitySingleton.GetEnemyEntity(currentEnemySpawnInfo.EnemyName);
+
+        //var enemy = ecb.Instantiate(enemyEntity);
+
+        //if (!SystemAPI.HasComponent<LocalTransform>(enemy)) { return; }
+
+        //var localTfm = SystemAPI.GetComponent<LocalTransform>(enemy);
+
+        //localTfm.Position = currentEnemySpawnInfo.SpawnPosition;
+
+        /*
+        Entity enemySpawnerEntity = SystemAPI.GetSingletonEntity<EnemyEntitySingletonData>();
+        state.EntityManager.SetSharedComponentManaged(enemySpawnerEntity, new SceneTag { SceneEntity = enemySpawnerEntity });
+
+        var enemyEntitySingleton = SystemAPI.GetSingleton<EnemyEntitySingletonData>();
+
+        if (!SystemAPI.HasSingleton<EnemySpawnerSingletonData>()) { return; }
+        var enemySpawnerSingleton = SystemAPI.GetSingleton<EnemySpawnerSingletonData>();
+
+        var enemySpawnInfos = enemySpawnerSingleton.enemySpawnInfos;
+        if (enemySpawnInfos.Length <= _currentSpawnNumber) { return; }
+
+        var currentEnemySpawnInfo = enemySpawnInfos[_currentSpawnNumber];
+
+        if (currentEnemySpawnInfo.CreationDelay > _elapsed) { return; }
+        _currentSpawnNumber++;
+
         var ecb = new EntityCommandBuffer(Allocator.Temp);
 
         // Entityと一緒にDataを取得する
-        foreach (var (enemySpawner, entity) in SystemAPI.Query<EnemySpawnerData>().WithEntityAccess())
+        foreach (var (enemySpawner, entity) in SystemAPI.Query<EnemySpawnerSingletonData>().WithEntityAccess())
         {
+            var enemyEntity = enemyEntitySingleton.GetEnemyEntity(currentEnemySpawnInfo.EnemyName);
+
+            // 敵を生成
+            var enemy = ecb.Instantiate(enemyEntity);
+
+            // ecbでDataをアタッチする
+            ecb.AddComponent(enemy, new EnemyTag());
+            ecb.AddComponent(enemy, new DirectionTravelData(enemySpawner.directionTravelType));
+
+            // LocalTransformを設定する
+            ecb.SetComponent(enemy, new LocalTransform
+            {
+                Position = currentEnemySpawnInfo.SpawnPosition,
+                Rotation = quaternion.identity,
+                Scale = 1.0f
+            });
+        }
+        */
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+        _elapsed += SystemAPI.Time.DeltaTime;
+
+        // Entityと一緒にDataを取得する
+        foreach (var (enemySpawner, entity) in SystemAPI.Query<EnemySpawnerSingletonData>().WithEntityAccess())
+        {
+            var enemySpawnInfos = enemySpawner.enemySpawnInfos;
+            if (enemySpawnInfos.Length <= _currentSpawnNumber) { return; }
+
+            var currentEnemySpawnInfo = enemySpawnInfos[_currentSpawnNumber];
+
+            if (currentEnemySpawnInfo.CreationDelay > _elapsed) { return; }
+            _currentSpawnNumber++;
+
+            Debug.Log("EnemyEntitySingletonDataが原因っぽい");
+            Debug.Log("SetSharedComponentManaged　SceneTagで大丈夫になったかとも思ったが駄目っぽい");
+            Debug.Log("そもそも何故このSingletonDataだけエラーになるのかが分からない");
+            Debug.Log("NoneをDynamicにしたり、ビルド時に固定をコメントアウトしても駄目だった");
+            //if (!SystemAPI.HasSingleton<EnemyEntitySingletonData>()) { return; }
+            //state.EntityManager.SetSharedComponentManaged(entity, new SceneTag { SceneEntity = entity });
+            //var enemyEntitySingleton = SystemAPI.GetSingleton<EnemyEntitySingletonData>();
+            //var enemyEntity = enemyEntitySingleton.GetEnemyEntity(currentEnemySpawnInfo.EnemyName);
+
             // 敵を生成
             var enemy = ecb.Instantiate(enemySpawner.enemyEntity);
 
@@ -56,8 +163,7 @@ public partial struct EnemySpawnerSystem : ISystem
             });
         }
 
-        // 変更を一括して適用
-        ecb.Playback(entityManager);
+        ecb.Playback(state.EntityManager);
         ecb.Dispose();
     }
 }
