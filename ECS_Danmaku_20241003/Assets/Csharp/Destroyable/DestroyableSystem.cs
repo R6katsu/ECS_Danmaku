69ac83d913c982a2faf2f2485062998c7d215c2e
20240710_PC <game.232e013@gmail.com>
2024-12-08 11:23:37 +0900

    20241208_Destroyableフォルダの中のソースコードをリファクタリング

--- ECS_Danmaku_20241003/Assets/Csharp/Destroyable/DestroyableSystem.cs
+++ ECS_Danmaku_20241003/Assets/Csharp/Destroyable/DestroyableSystem.cs
@@ -1,41 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Burst;
 using Unity.Entities;
+
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static EnemyHelper;
+#endif
+
+// リファクタリング済み
 
 /// <summary>
-/// 削除処理
+/// Entityの削除処理
 /// </summary>
 [BurstCompile]
 public partial struct DestroyableSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
-        // ecbを取得
+        // EndSimulationEntityCommandBufferSystemで最後に反映
         var ecbSystem = state.World.GetOrCreateSystemManaged<EndSimulationEntityCommandBufferSystem>();
         var ecb = ecbSystem.CreateCommandBuffer().AsParallelWriter();
 
-        // 削除処理を呼び出す
+        // Entityの削除処理
         var job = new DestroyJob
         {
             ecb = ecb
         };
-
-        // ジョブをスケジュール
         var handle = job.ScheduleParallel(state.Dependency);
 
-        // 依存関係を伝える
+        // 依存関係を追加
         ecbSystem.AddJobHandleForProducer(handle);
 
-        // 次のシステム更新前に依存関係を更新
+        // 依存関係を更新
         state.Dependency = handle;
     }
 }
 
 /// <summary>
-/// 削除処理
+/// Entityの削除処理
 /// </summary>
 [BurstCompile]
 partial struct DestroyJob : IJobEntity
