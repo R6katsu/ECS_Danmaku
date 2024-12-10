a66e56202a0c49c9e7f4d399cef44f135ada5914
20240710_PC <game.232e013@gmail.com>
2024-12-11 08:05:16 +0900

    20241211_NumericExtensionsフォルダの中のソースコードをリファクタリング

--- ECS_Danmaku_20241003/Assets/Csharp/NumericExtensions/NumericExtensions.cs
+++ ECS_Danmaku_20241003/Assets/Csharp/NumericExtensions/NumericExtensions.cs
@@ -6,6 +6,8 @@ using System.Collections;
 using System.Collections.Generic;
 #endif
 
+// リファクタリング済み
+
 /// <summary>
 /// 数値の拡張関数
 /// </summary>
@@ -15,12 +17,56 @@ public static class NumericExtensions
     private const int HALVE_NUMBER = 2;
 
     /// <summary>
+    /// intの拡張関数。<br/>
+    /// 半分の値を返す
+    /// </summary>
+    /// <param name="value">半分に割る値</param>
+    /// <returns>半分の値</returns>
+    public static int Halve(this int value)
+    {
+        return value / HALVE_NUMBER;
+    }
+
+    /// <summary>
+    /// int2の拡張関数。<br/>
+    /// 半分の値を返す
+    /// </summary>
+    /// <param name="value">半分に割る値</param>
+    /// <returns>半分の値</returns>
+    public static int2 Halve(this int2 value)
+    {
+        return value / HALVE_NUMBER;
+    }
+
+    /// <summary>
+    /// int3の拡張関数。<br/>
+    /// 半分の値を返す
+    /// </summary>
+    /// <param name="value">半分に割る値</param>
+    /// <returns>半分の値</returns>
+    public static int3 Halve(this int3 value)
+    {
+        return value / HALVE_NUMBER;
+    }
+
+    /// <summary>
     /// floatの拡張関数。<br/>
     /// 半分の値を返す
     /// </summary>
     /// <param name="value">半分に割る値</param>
     /// <returns>半分の値</returns>
     public static float Halve(this float value)
+    {
+        return value / HALVE_NUMBER;
+    }
+
+    /// <summary>
+    /// float2の拡張関数。<br/>
+    /// 半分の値を返す
+    /// </summary>
+    /// <param name="value">半分に割る値</param>
+    /// <returns>半分の値</returns>
+    public static float2 Halve(this float2 value)
     {
         return value / HALVE_NUMBER;
     }
